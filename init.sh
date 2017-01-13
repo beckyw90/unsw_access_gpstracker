103e3f6cb5f07e6a8e076c4ca58dee454024c5dd
Dominic <z5024963@cse.unsw.edu.au>
2017-01-13 15:47:28 +1100

    init.sh: fix permissions

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 chmod o+rw .
 #chmod a+rw *
 python3.5 query.py > /dev/null
-chmod a+rw activity.sqlite3
+chmod a+rw activity.sqlite3 index.html
 chmod -R a+rX css script images
-chmod 755 *.py
+chmod 755 *.py .htaccess
